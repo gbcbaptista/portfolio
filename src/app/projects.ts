c2cc32ef7f122887e25e33628407a189702d4d42
Gabriel Baptista Cerqueira <66979159+gbcbaptista@users.noreply.github.com>
2025-07-09 19:01:22 -0300

    feat: typo on kaggle project

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -27,7 +27,7 @@ const projectsList = [
     id: 2,
     title: "Kaggle Competitions Journey",
     description:
-      "A deep dive into ML fundamentals where I build, debug, and refine algorithms from scratch. In the Titanic challenge, I implemented Logistic Regression using only NumPy, engineered complex features, and troubleshot numerical stability issues by creating a custom feature scaler, ultimately matching Scikit-learn's benchmark performance.",
+      "A deep dive into ML fundamentals where I build, debug, and refine algorithms from scratch. In the Titanic challenge, I implemented Logistic Regression using only NumPy, engineered complex features, and troubleshot numerical stability issues by implementing a feature scaler, ultimately matching Scikit-learn's benchmark performance.",
     techStack: ["Python", "NumPy", "Pandas", "Matplotlib", "Scikit-learn"],
     category: "AI/ML",
     status: "Development" as const,
